da59ca55c8c6a652dc3601438ebeb6861b4fda00
Romain PEDRERO <romain.pedrero[at]mines-ales.org>
2024-01-11 14:36:45 +0100

    Created repositories interfaces.

---
+++ WikY.Repositories.Contracts/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace WikY.Repositories.Contracts
+{
+    public interface IRepository<T, ID>
+    {
+        public T Create(T entity);
+        public T GetById(ID id);
+        public ICollection<T> GetAll();
+        public void Update(T entity);
+        public void Delete(T entity);
+    }
+}
