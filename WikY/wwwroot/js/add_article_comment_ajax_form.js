dd84d6a714cd2ffedb32c8d906cd6cdc52ac224f
Romain PEDRERO <romain.pedrero[at]mines-ales.org>
2024-01-20 21:35:28 +0100

    Moved Javascript code in several files.

---
+++ WikY/wwwroot/js/add_article_comment_ajax_form.js
@@ -0,0 +1,92 @@
+﻿let globalFormErrorElement = $('#ajax-global-form-error-placeholder');
+
+let articleIdInputElement = $('#ajax-article-id-input');
+let authorInputElement = $('#ajax-author-input');
+let contentInputElement = $('#ajax-content-input');
+
+let ajaxAuthorValidationElement = $('#ajax-author-validation');
+let ajaxContentValidationElement = $('#ajax-content-validation');
+
+$('#create-comment-ajaxversion-form').submit(function (e) {
+    e.preventDefault();
+
+    $.post('/Comment/CreateAjax',
+        {
+            ArticleId: articleIdInputElement.val(),
+            Author: authorInputElement.val(),
+            Content: contentInputElement.val()
+        },
+        function (data) {
+            let noCommentTextElement = $('#no-comment');
+            if (noCommentTextElement) {
+                noCommentTextElement.remove();
+            }
+
+            $('#article-comments').prepend(data);
+
+            authorInputElement.val(null);
+            contentInputElement.val(null);
+
+            globalFormErrorElement.children().remove();
+            globalFormErrorElement.addClass('hidden');
+        })
+        .fail(function (response) {
+            if (response.status === 400) {
+                let responseJSON = response.responseJSON;
+
+                if (responseJSON['Author']) {
+                    ajaxAuthorValidationElement.text(responseJSON['Author']);
+                    ajaxAuthorValidationElement.removeClass('hidden');
+                }
+
+                if (responseJSON['Content']) {
+                    ajaxContentValidationElement.text(responseJSON['Content']);
+                    ajaxContentValidationElement.removeClass('hidden');
+                }
+            }
+            else if (response.status === 500) {
+                globalFormErrorElement.append('<p>An unexpected error has occurred when attempting to send your comment. Try again later.</p>');
+                globalFormErrorElement.removeClass('hidden');
+            }
+        });
+});
+
+authorInputElement.on('input', function (e) {
+    if (!e.target.value.trim()) {
+        ajaxAuthorValidationElement.text('The Author field is required.');
+        ajaxAuthorValidationElement.removeClass('hidden');
+    }
+    else {
+        ajaxAuthorValidationElement.text(null);
+        ajaxAuthorValidationElement.addClass('hidden');
+
+        if (e.target.value.length > 30) {
+            ajaxAuthorValidationElement.text('The field Author must be a string or array type with a maximum length of \'30\'.');
+            ajaxAuthorValidationElement.removeClass('hidden');
+        }
+        else {
+            ajaxAuthorValidationElement.text(null);
+            ajaxAuthorValidationElement.addClass('hidden');
+        }
+    }
+});
+
+contentInputElement.on('input', function (e) {
+    if (!e.target.value.trim()) {
+        ajaxContentValidationElement.text('The Content field is required.');
+        ajaxContentValidationElement.removeClass('hidden');
+    }
+    else {
+        ajaxContentValidationElement.text(null);
+        ajaxContentValidationElement.addClass('hidden');
+
+        if (e.target.value.length > 1000) {
+            ajaxContentValidationElement.text('The field Content must be a string or array type with a maximum length of \'1000\'.');
+            ajaxContentValidationElement.removeClass('hidden');
+        }
+        else {
+            ajaxContentValidationElement.text(null);
+            ajaxContentValidationElement.addClass('hidden');
+        }
+    }
+});
---
+++ WikY/wwwroot/js/delete_article_from_list.js
@@ -0,0 +1,33 @@
+﻿let articleDeleteModal = new bootstrap.Modal(document.getElementById('delete-article-modal'), {
+    keyboard: false
+});
+
+let modalArticleToDeleteTitleElement = $('#article-to-delete-title');
+
+let selectedArticleId = null;
+
+function unselectCurrentArticle() {
+    selectedArticleId = null;
+    modalArticleToDeleteTitleElement.text('');
+}
+
+function showDeleteModal(articleId) {
+    selectedArticleId = articleId;
+
+    modalArticleToDeleteTitleElement.text($('#article-' + articleId + '>td>a').text());
+
+    articleDeleteModal.show();
+}
+
+function deleteSelectedArticle() {
+    $.ajax({
+        url: '/Article/DeleteJson/' + selectedArticleId,
+        type: 'DELETE',
+        success: function (result) {
+            $('#article-' + selectedArticleId).remove();
+        },
+
+    }).done(function () {
+        unselectCurrentArticle();
+    });
+}
